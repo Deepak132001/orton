dc5dc6cd13f86b6a2513350ca26caf68a6c42e60
deepakrandhawa191@gmail.com <deepakrandhawa191@gmail.com>
2024-12-29 16:59:19 +0700

    deploy

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -40,7 +40,7 @@ export const register = async (req, res, next) => {
     const token = jwt.sign(
       { userId: user._id },
       process.env.JWT_SECRET,
-      { expiresIn: '24h' }
+      { expiresIn: '90d' }
     );
 
     res.status(201).json({
@@ -104,7 +104,7 @@ export const login = async (req, res, next) => {
     const token = jwt.sign(
       { userId: user._id },
       process.env.JWT_SECRET,
-      { expiresIn: '24h' }
+      { expiresIn: '90d' }
     );
 
     res.json({
@@ -120,76 +120,3 @@ export const login = async (req, res, next) => {
     next(error);
   }
 };
-// // server/src/controllers/auth.controller.js
-// import jwt from 'jsonwebtoken';
-// import { User } from '../models/user.model.js';
-
-// export const register = async (req, res, next) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     // Check if user exists
-//     const existingUser = await User.findOne({ email });
-//     if (existingUser) {
-//       return res.status(400).json({ message: 'User already exists' });
-//     }
-
-//     // Create new user
-//     const user = new User({ email, password });
-//     await user.save();
-
-//     // Generate token
-//     const token = jwt.sign(
-//       { userId: user._id },
-//       process.env.JWT_SECRET,
-//       { expiresIn: '24h' }
-//     );
-
-//     res.status(201).json({
-//       message: 'User created successfully',
-//       token,
-//       user: {
-//         id: user._id,
-//         email: user.email,
-//       },
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
-// export const login = async (req, res, next) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     // Find user
-//     const user = await User.findOne({ email });
-//     if (!user) {
-//       return res.status(401).json({ message: 'Invalid credentials' });
-//     }
-
-//     // Check password
-//     const isMatch = await user.comparePassword(password);
-//     if (!isMatch) {
-//       return res.status(401).json({ message: 'Invalid credentials' });
-//     }
-
-//     // Generate token
-//     const token = jwt.sign(
-//       { userId: user._id },
-//       process.env.JWT_SECRET,
-//       { expiresIn: '24h' }
-//     );
-
-//     res.json({
-//       message: 'Login successful',
-//       token,
-//       user: {
-//         id: user._id,
-//         email: user.email,
-//       },
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// };

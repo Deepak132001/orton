858c5cc33c72a4e3ea14bb37283705e8dbc849ef
deepakrandhawa191@gmail.com <deepakrandhawa191@gmail.com>
2025-01-09 22:07:08 +0700

    changes

--- server/src/app.js
+++ server/src/app.js
@@ -266,7 +266,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:5173',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'https://www.ortonai.com',
 ];
 
 app.use(cors({
